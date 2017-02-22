6a782c02765f3799321234add0e01aaa980ba3df
Marcus Crane <marcus@thingsima.de>
2017-02-22 16:14:47 +1300

    Basic app works

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 const imdb = require('imdb-api')
+const WunderlistSDK = require('wunderlist')
+const env = require('dotenv').config()
 
-imdb.get('Lost').then(series => {
-    series.episodes((err, eps) => {
-        for (i in eps) {
-            if (eps[i].episode < 10) {
-                console.log(`S${eps[i].season}E0${eps[i].episode}`)
-            } else {
-                console.log(`S${eps[i].season}E${eps[i].episode}`)
+const wunderlistAPI = new WunderlistSDK({
+    'accessToken': process.env.WUNDERLIST_ACCESS_TOKEN,
+    'clientID': process.env.WUNDERLIST_CLIENT_ID
+})
+
+let data = []
+
+imdb.get(process.argv[2]).then(series => {
+    series.episodes((err, episodes) => {
+        if (!err) {
+            for (i in episodes) {
+                if (episodes[i].episode < 10) {
+                    data.push(`S${episodes[i].season}E0${episodes[i].episode}`)
+                } else {
+                    data.push(`S${episodes[i].season}E${episodes[i].episode}`)
+                }
             }
+
+            addTasks()
+        } else {
+            console.log('Episode fetching went haywire!', err)
         }
     })
 })
+.catch(err => {
+    console.log(err)
+})
+
+const addTasks = () => {
+    wunderlistAPI.http.tasks.create({
+        'list_id': parseInt(process.env.WUNDERLIST_TVSERIES_LIST),
+        'title': process.argv[2]
+    })
+    .done((taskData, statusCode) => {
+        console.log('Created task. Adding episodes...', statusCode, taskData)
+
+        for (i in data) {
+            addSubtask(taskData.id, data[i])
+        }
+    })
+    .fail((resp, code) => {
+        console.log(code, resp)
+    })
+}
+
+const addSubtask = (taskID, episodeTitle) => {
+    wunderlistAPI.http.subtasks.create({
+        'task_id': taskData,
+        'title': episodeTitle
+    })
+}
